179d682597396a1ce0802747c7d827bc4ed7cd55
Marc Hermans <oriondevelopment@outlook.com>
2019-02-15 14:58:40 +0100

    Reworking the first provider.

--- api/src/main/java/com/ldtteam/jvoxelizer/block/state/IBlockState.java
+++ api/src/main/java/com/ldtteam/jvoxelizer/block/state/IBlockState.java
@@ -2,6 +2,18 @@ package com.ldtteam.jvoxelizer.block.state;
 
 import com.ldtteam.jvoxelizer.util.nbt.INBTSerializable;
 
+/**
+ * Represents the state of a Block in the game.
+ */
 public interface IBlockState extends INBTSerializable
 {
+
+    /**
+     * Creates a new instance of the {@link IBlockState}
+     * @return a new {@link IBlockState}
+     */
+    static IBlockState create()
+    {
+        return BlockStateProviderHolder.getInstance().provide();
+    }
 }

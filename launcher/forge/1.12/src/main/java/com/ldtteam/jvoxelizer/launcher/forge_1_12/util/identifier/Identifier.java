0679ea984bc8cd6c062d40669d5d4882743b5f32
Marc Hermans <oriondevelopment@outlook.com>
2019-03-31 15:30:45 +0200

    Adding equals and hashcode to Identifier for use in maps etc.

--- launcher/forge/1.12/src/main/java/com/ldtteam/jvoxelizer/launcher/forge_1_12/util/identifier/Identifier.java
+++ launcher/forge/1.12/src/main/java/com/ldtteam/jvoxelizer/launcher/forge_1_12/util/identifier/Identifier.java
@@ -3,6 +3,8 @@ package com.ldtteam.jvoxelizer.launcher.forge_1_12.util.identifier;
 import com.ldtteam.jvoxelizer.util.identifier.IIdentifier;
 import net.minecraft.util.ResourceLocation;
 
+import java.util.Objects;
+
 /**
  * Identifier class for holding resource locations
  */
@@ -37,5 +39,28 @@ public class Identifier implements IIdentifier {
             return (IIdentifier) location;
 
         return new Identifier(location);
+    }
+
+    @Override
+    public boolean equals(final Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (!(o instanceof Identifier))
+        {
+            return false;
+        }
+
+        final Identifier that = (Identifier) o;
+
+        return Objects.equals(forgeIdentifier, that.forgeIdentifier);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return forgeIdentifier != null ? forgeIdentifier.hashCode() : 0;
     }
 }

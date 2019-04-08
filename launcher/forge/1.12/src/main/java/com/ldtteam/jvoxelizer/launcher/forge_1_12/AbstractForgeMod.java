8a022e8696038f2bd6a8e78229e2a7e427d3a0de
Marc Hermans <oriondevelopment@outlook.com>
2019-04-08 15:20:00 +0200

    Attempting with a double supplier.

--- launcher/forge/1.12/src/main/java/com/ldtteam/jvoxelizer/launcher/forge_1_12/AbstractForgeMod.java
+++ launcher/forge/1.12/src/main/java/com/ldtteam/jvoxelizer/launcher/forge_1_12/AbstractForgeMod.java
@@ -1,22 +1,34 @@
 package com.ldtteam.jvoxelizer.launcher.forge_1_12;
 
 import com.ldtteam.jvoxelizer.launcher.forge_1_12.core.IForgeJVoxelizerSetupProxy;
+import com.ldtteam.jvoxelizer.launcher.forge_1_12.proxy.ClientProxy;
+import com.ldtteam.jvoxelizer.launcher.forge_1_12.proxy.CommonProxy;
+import com.ldtteam.jvoxelizer.launcher.forge_1_12.util.distribution.Distribution;
+import com.ldtteam.jvoxelizer.launcher.forge_1_12.util.distribution.executor.DistributionExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
+
+import java.util.function.Supplier;
 
 public abstract class AbstractForgeMod
 {
 
     private Logger logger;
 
-    @SidedProxy(clientSide = "com.ldtteam.jvoxelizer.launcher.forge_1_12.proxy.ClientProxy", serverSide = "com.ldtteam.jvoxelizer.launcher.forge_1_12.proxy.CommonProxy")
     private IForgeJVoxelizerSetupProxy setupProxy;
 
     @Mod.EventHandler
     public void preInit(final FMLPreInitializationEvent event)
     {
+        final Supplier<IForgeJVoxelizerSetupProxy> proxySupplier = DistributionExecutor.getInstance().runOn(
+          () -> ClientProxy::new,
+          () -> CommonProxy::new
+        );
+
+        this.setupProxy = proxySupplier.get();
+
         logger = event.getModLog();
         logger.info("Initializing JVoxelizer.");
 
